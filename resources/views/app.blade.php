90245d8840b3c3645d872e7745af4fe6f3505386
daniel-villada <cdvillada6@misena.edu.co>
2023-08-19 17:02:28 -0500

    ✍feat: agregar tailwind css y yarn al proyecto

---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>JG Financies</title>
+</head>
+
+<body>
+    <h1 class="text-3xl text-purple-500 font-bold">Este es el punto de entrada del proyecto jgfinancies</h1>
+</body>
+
+</html>
