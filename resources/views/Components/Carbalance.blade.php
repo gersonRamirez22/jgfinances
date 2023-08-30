7d04cec9cf5ba0a9ae7b58022c20e48100f92a76
Yessica Herrera <lissethagudelo4321@gmail.com>
2023-08-30 18:43:00 -0500

    se agrega la card_balance al dashborad

---
+++ resources/views/Components/Carbalance.blade.php
@@ -0,0 +1,34 @@
+<div class="  flex flex-col rounded-xl bg-[#FFF] w-52 h-72 py-6 px-4 gap-4">
+    <div class=" flex flex-col gap-y-2">
+        <p class="text-[#D0D0D0] text-sm font-normal  ">Total balance</p>
+        <p class="text-xl font-medium gap-y-2">$103.120,23</p>
+        <div class="bg-[#D0D0D0] h-px w-40"></div>
+    </div>
+   
+
+    <div>
+       <div class="flex justify-between items-center mb-4">
+         <p class="font-medium text-sm ">My Accounts</p>
+         <ion-icon class="h-5 w-5" name="create-outline"></ion-icon>
+       </div>
+       
+        <div class="mb-2 text-[#D0D0D0] text-xs flex justify-between">
+            <p>account 1</p>
+            <p>51.105,13</p>
+        </div >
+        <div class="text-[#D0D0D0] text-xs flex justify-between" >
+            <p>account 2</p>
+            <p>22.010,05</p> 
+        </div>
+        <div class="mt-2 text-[#D0D0D0] text-xs flex justify-between">
+            <p>account 3</p>
+            <p>30.005,05</p>
+        </div>
+        <div class="bg-[#805CFF]  flex  justify-center items-center rounded-lg p-2 text-xs text-[#FFF] mt-4">
+        <button type="submit" > Create Account</button>
+        </div>
+        
+
+    </div>
+
+</div>
