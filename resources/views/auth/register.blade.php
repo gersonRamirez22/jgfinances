0d73a689a9eca70fa029c8e403131bfb0d3218f4
daniel-villada <cdvillada6@misena.edu.co>
2023-09-08 19:04:39 -0500

    chore: add meta tag for resolved an issue with styles

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     @vite('resources/css/app.css')
     <title>Register</title>
     <link rel="stylesheet" href="style.css">
@@ -14,21 +15,22 @@
     <div class="grid items-center md:grid-cols-2 h-[100vh]">
         <div class="p-10">
             <div class="-mt-px">
-                <x-logo/>
+                <x-logo />
             </div>
             <div class="mt-2">
                 <h1 class="font-sans text-3xl">Create Account</h1>
                 <p class="font-sans text-base text-gray-500">take control of your finances and make your dreams come true. Start for free</p>
             </div>
             <div class="mt-8">
-                <p>Already a member? <a  href="#" class=" font-medium text-[#754EFF]">Log in</a></p>
+                <p>Already a member? <a href="#" class=" font-medium text-[#754EFF]">Log in</a></p>
             </div>
             <div>
-                <x-RegisterForm/>
+                <x-RegisterForm />
             </div>
-            <x-button buttonTitle="Sign Up"/>
+            <x-button buttonTitle="Sign Up" />
         </div>
-        <x-bannerImage/>
+        <x-bannerImage />
     </div>
 </body>
+
 </html>
